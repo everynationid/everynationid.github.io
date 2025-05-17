70a4164b59e611a8cd365716ca0a364119a9831f
everynationid <ict@everynation.id>
2025-05-17 19:12:47 +0700

    update

--- auth/google-auth.js
+++ auth/google-auth.js
@@ -1,6 +1,6 @@
 function onSignIn(response) {
   const email = jwt_decode(response.credential).email;
-  const allowedEmails = ['youremail@gmail.com']; // <-- your email
+  const allowedEmails = ['ict@everynation.id']; // <-- your email
   if (allowedEmails.includes(email)) {
     document.getElementById('cms').style.display = 'block';
     document.querySelector('.g_id_signin').style.display = 'none';
